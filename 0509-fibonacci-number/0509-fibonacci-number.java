08f267a9a96687b678291ff81f49c4f4ab633c6a
Avani Singh <109615193+Avani-Singh-2003@users.noreply.github.com>
2024-08-21 06:16:31 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (24.46%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,28 @@
 class Solution {
     public int fib(int n) {
-        if(n==0)
+        // if(n==0)
+        //     return 0;
+        // if(n==1)
+        //     return 1;
+        // if(n>1)
+        //     return fib(n-1)+fib(n-2);
+        // return 0;
+        
+         if (n == 0) {
             return 0;
-        if(n==1)
+        } else if (n == 1) {
             return 1;
-        if(n>1)
-            return fib(n-1)+fib(n-2);
-        return 0;
+        }
+
+        int prev2 = 0;  
+        int prev1 = 1;  
+
+        for (int i = 2; i <= n; i++) {
+            int current = prev1 + prev2;  
+            prev2 = prev1;  
+            prev1 = current;  
+        }
+
+        return prev1;
     }
 }
