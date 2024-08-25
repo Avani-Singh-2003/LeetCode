6613f1930022681f6efa338c1729a014869de5ba
Avani Singh <109615193+Avani-Singh-2003@users.noreply.github.com>
2024-08-26 00:09:10 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (65.19%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-       if (n == 1)
-            return 1;
-        if (n == 2)
-            return 2;
-
-        int prev2 = 1;  // Number of ways to reach step 1
-        int prev1 = 2;  // Number of ways to reach step 2
-
-        for (int i = 3; i <= n; i++) {
-            int current = prev1 + prev2;  // Number of ways to reach step i
-            prev2 = prev1;  // Update prev2 to the value of prev1
-            prev1 = current;  // Update prev1 to the value of current
+        if(n==0 || n==1) return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-
-        return prev1;
-
+        return dp[n];
     }
 }
