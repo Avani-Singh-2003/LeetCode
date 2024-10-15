008d5233a9ae1d97b35be39be26b456d575b1fce
Avani Singh <109615193+Avani-Singh-2003@users.noreply.github.com>
2024-10-15 17:59:57 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (89.07%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,6 +1,17 @@
 class Solution {
     public String toLowerCase(String s) {
-        String n = s.toLowerCase();
-        return n;
+         
+       // return s.toLowerCase();
+        
+        char[] c = s.toCharArray();
+        
+        for(int i=0;i<c.length;i++){
+            if(c[i] >= 65 && c[i]<= 90){
+            c[i] = (char) (c[i] + 32);
+                    }
+        
+        
+        }
+        return String.valueOf(c);
     }
 }
